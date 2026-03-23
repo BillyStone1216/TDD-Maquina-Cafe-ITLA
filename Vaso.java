8c4a46a55970623726f684cd783edfe21afc344f
Yoskal Contreras <133520785+BillyStone1216@users.noreply.github.com>
2026-03-23 11:52:23 -0400

    Add Vaso class with inventory management methods

---
+++ Vaso.java
@@ -0,0 +1,21 @@
+public class Vaso {
+    private int cantidadVasos;
+    private int contenido;
+
+    public Vaso(int cantidadVasos, int contenido) {
+        this.cantidadVasos = cantidadVasos;
+        this.contenido = contenido;
+    }
+
+    public boolean hasVasos(int cantidad) {
+        return this.cantidadVasos >= cantidad;
+    }
+
+    public void giveVasos(int cantidad) {
+        this.cantidadVasos -= cantidad; // Restamos la cantidad del inventario
+    }
+
+    public int getCantidadVasos() {
+        return cantidadVasos;
+    }
+}
